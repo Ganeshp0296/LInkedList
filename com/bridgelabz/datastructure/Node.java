749a059f6854c8e8b5ee32f17b827c18caa4e743
Ganeshp0296 <Ganeshp457@gmail.com>
2022-06-10 16:19:09 +0530

    Adding UC1

---
+++ com/bridgelabz/datastructure/Node.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.datastructure;
+
+public class Node<K> {
+
+   K key;
+
+   Node<K> next; // default is null
+
+    public Node() {
+    }
+
+    public Node(K key) {
+        this.key = key;
+    }
+}
